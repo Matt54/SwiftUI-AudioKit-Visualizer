fe4007988cf499284d9e6cd1bba557d1394c3d5c
Matt Pfeiffer <mattpfeiffer54@gmail.com>
2020-06-27 09:49:52 -0500

    Initial Commit

---
+++ Visualizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Visualizer
+//
+//  Created by Macbook on 6/27/20.
+//  Copyright © 2020 Matt Pfeiffer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
